8d7c0554aafa2a1dcecf91928a315431d16794f5
Abraham Terfie <abrahamterfie4563@gmail.com>
2021-08-10 16:40:49 +0300

    feat : splash screen

--- Screens/AuthScreen/SplashScreen.js
+++ Screens/AuthScreen/SplashScreen.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React , {useState , useEffect} from 'react';
 import {View, Text, Image, TouchableOpacity} from 'react-native';
 import {Button} from 'react-native-paper';
 import * as Animatable from 'react-native-animatable';
 import {ActivityIndicator, Colors} from 'react-native-paper';
 export default function SplashScreen({navigation}) {
+  //set timer to load log in screen 
+  setInterval(()=>{
+    navigation.navigate('LoginScreen')
+  }, 3000)
+
   return (
-    <View>
-      <View style={styles.imagecontainer}>
+    <View style={styles.parentContainer}>
+      <View style={styles.logoContainer}>
         <TouchableOpacity>
-          <Image
-            style={{
-              height: 300,
-              width: 250,
-            }}
-            source={require('../../Images/LOGO.png')}
-          />
+          <View style={styles.imagecontainer}>
+            <Image
+              style={{
+                height: 300,
+                width: 250,
+              }}
+              source={require('../../Images/LOGO.png')}
+            />
+          </View>
         </TouchableOpacity>
       </View>
-
       <View>
         <ActivityIndicator animating={true} color="#009F93" size={75} />
       </View>
@@ -25,14 +31,27 @@ export default function SplashScreen({navigation}) {
   );
 }
 
+
+
 const styles = {
   imagecontainer: {
     justifyContent: 'center',
     alignItems: 'center',
     margin: 200,
+  },
+  logoContainer: {
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 300,
+    width: 290,
+    paddingBottom: 50,
+    alignSelf: 'center',
+
+    borderRadius:60,
+    borderBottomColor:'#222222',
 
     //shadows
-
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
@@ -42,5 +61,10 @@ const styles = {
     shadowRadius: 16.0,
 
     elevation: 24,
+  },
+  parentContainer: {
+    height: '100%',
+    justifyContent: 'space-around',
+    alignItems: 'center',
   },
 };
