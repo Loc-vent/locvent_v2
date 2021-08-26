cdaedb6694d6de7857b4531bda62513b5accdf9c
Abraham Terfie <abrahamterfie4563@gmail.com>
2021-08-26 22:34:31 +0300

    feat : new state

--- Context/Store.js
+++ Context/Store.js
@@ -1,8 +1,88 @@
 import React, {useState, createContext} from 'react';
 
 const initState = {
-  activeStation: {},
-  admin: false,
+  broadCast: {
+    id: '',
+    message: '',
+  },
+  centralAdmin: {
+    id: '',
+    isAdmin: false,
+  },
+  experts: {
+    id: '',
+    lastLogin: '',
+    region: '',
+    woreda: '',
+  },
+  generalUsers: {
+    id: '',
+    woreda: '',
+    region: '',
+    report: '',
+  },
+  locust: {
+    id: '',
+    locustColor: '',
+    locustImageLocation: '',
+    locustDescription: '',
+    locustTypeId: '',
+    licustPhaseId: '',
+  },
+  locustPhase: {
+    id: '',
+    name: '',
+    locustPhaseDescription: '',
+  },
+  locustType: {
+    id: '',
+    name: '',
+  },
+  messages: {
+    userId: '',
+    message: '',
+    date: '',
+  },
+  news: {
+    id: '',
+    newsHeader: '',
+    date: '',
+    newsContent: '',
+    newsPhotoLocation: '',
+    newscategory: '',
+  },
+
+  newsCategory: {
+    id: '',
+    categoryName: '',
+  },
+  region: {
+    id: '',
+    regionName: '',
+  },
+  report: {
+    id: '',
+    reporterId: '',
+    reportLatitude: '',
+    reportLongitude: '',
+    reportPhotoLocation: '',
+    DdetectedLocust: '',
+  },
+  user: {
+    id: '',
+    userName: '',
+    phoneNumber: '',
+    firstName: '',
+    lastName: '',
+    gender: '',
+    centralAdmin: '',
+    experts: '',
+    generalUsers: '',
+    role: '',
+    lastLogin: '',
+    notification: '',
+  },
+
 };
 export const Context = createContext(initState);
 export default function Store({children}) {
